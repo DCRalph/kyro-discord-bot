54a2861d01b65653087b89dc4b57f89153c33c5c
DCRalph <43304937+DCRalph@users.noreply.github.com>
2022-04-20 23:47:38 +1200

    e

--- commands/L Brandon.js
+++ commands/L Brandon.js
@@ -5,24 +5,32 @@ import Discord from 'discord.js'
 import db from '../db.js'
 
 const create = (client) => {
-  new command.Leagcy(
-    client,
-    ['does Brandon have a massive dump'],
-    'yes',
-    async (message) => {
-      let [first, ...rest] = message.content.toLowerCase().split(' ')
-      let cmd = rest.join(' ').trim()
+  new command.Leagcy(client, ['brandon ='], 'yes', async (message) => {
+    let [first, ...rest] = message.content
+      .toLowerCase()
+      .replace('brandon =', '')
+      .split(' ')
+    let cmd = rest.join(' ').trim()
 
-      const requis = () => {
-        message.channel.send({
-          content: ':requis:',
-        })
-      }
-
-      if (cmd === 'yes') {
-        requis()
-      }
+    const requis = () => {
+      message.channel.send({
+        files: [
+          'https://cdn.discordapp.com/attachments/700110467049193523/965923204407365692/avatars-qFNWvHxdUwvGWEQZ-taPVRA-t240x240.jpg',
+        ],
+      })
     }
-  )
+
+    console.log(cmd)
+
+    if (cmd.includes('thicc')) {
+      return requis()
+    }
+
+    if (cmd.includes('massive') && cmd.includes('dumpy')) {
+      return requis()
+    }
+
+    message.channel.send('wrong')
+  })
 }
 export { create }
